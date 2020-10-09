5862cee03d091baedd9a67092d9fcf37e71a9834
Gabriel <Gabriel@DESKTOP-H2GRKFL.local>
2020-10-08 21:56:22 -0300

    Proxima semana

--- src/controllers/UsuarioController.java
+++ src/controllers/UsuarioController.java
@@ -4,10 +4,11 @@ import connection.Conexao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import models.Candidato;
+import models.Usuario;
 
 public class UsuarioController {
 
-    public boolean validarLogin(String login, String senha) {
+    public Usuario validarLogin(String login, String senha) {
         try {
             Conexao.abreConexao();
             ResultSet rs = null;
@@ -18,25 +19,34 @@ public class UsuarioController {
             SQL += " WHERE login = '" + login + "'";
             SQL += " AND senha = md5('" + senha + "')";
 
+            Usuario objUsuario;
             try {
+
                 System.out.println("Vai Executar Conexão em buscar");
                 rs = Conexao.stmt.executeQuery(SQL);
                 System.out.println("Executou Conexão em buscar");
 
                 if (rs.next() == true) {
-                    return true;
+                    objUsuario = new Usuario();
+
+                    objUsuario.setId(rs.getInt("id"));
+                    objUsuario.setLogin(rs.getString("login"));
+                    objUsuario.setNome(rs.getString("nome"));
+
+                    return objUsuario;
+
                 }
             } catch (SQLException ex) {
                 System.out.println("ERRO de SQL: " + ex.getMessage().toString());
-                return false;
+                return null;
             }
 
         } catch (Exception e) {
             System.out.println("ERRO: " + e.getMessage().toString());
-            return false;
+            return null;
         }
 
-        return false;
+        return null;
     }
 
 }
---
+++ src/models/Usuario.java
@@ -0,0 +1,33 @@
+package models;
+
+public class Usuario {
+
+    private int id;
+    private String login;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+}
